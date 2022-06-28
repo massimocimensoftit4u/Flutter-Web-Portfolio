eaa49471815b9507ce31810fa7c9d5d7487eafe2
Massimo Cimen <m.cimen@metacarp.de>
2022-06-28 17:33:31 +0200

    test

--- lib/pages/home/components/about.dart
+++ lib/pages/home/components/about.dart
@@ -47,7 +47,7 @@ class _AboutSectionState extends State<AboutSection> {
                         height: 15.0,
                       ),
                       Text(
-                        "About Me",
+                        "Über uns",
                         style: GoogleFonts.josefinSans(
                           fontWeight: FontWeight.w900,
                           height: 1.3,
@@ -58,7 +58,7 @@ class _AboutSectionState extends State<AboutSection> {
                         height: 25.0,
                       ),
                       Text(
-                        "I'm Agnel Selvan, A Flutter and iOS Developer and Technical Blog Writer",
+                        "Hier stellen wir uns einmal vor",
                         style: GoogleFonts.josefinSans(
                           fontWeight: FontWeight.bold,
                           height: 1.3,
@@ -69,7 +69,7 @@ class _AboutSectionState extends State<AboutSection> {
                         height: 10.0,
                       ),
                       const Text(
-                        "I have done my Computer Science graduation at Xavier's Institute of Engineering on 2021. I have been developing Mobile Apps for more than 1 years now. I have worked as a Team and as an Indivual in various organization and launched the apps in Playstore as well as in Appstore. In my free time I use to write Technical Blog in Medium. Always love to learn new technologies and to succeed in an environment of growth and excellence and earn a job which provides me job satisfaction and self-development and help me achieve personal as well as organisational goals.",
+                        "Hier kann man einen langen Text hinterlegen, um uns zu beschreiben. Hier kann man einen langen Text hinterlegen, um uns zu beschreiben. Hier kann man einen langen Text hinterlegen, um uns zu beschreiben. Hier kann man einen langen Text hinterlegen, um uns zu beschreiben. Hier kann man einen langen Text hinterlegen, um uns zu beschreiben. ",
                         style: TextStyle(
                           color: kCaptionColor,
                           height: 1.5,
@@ -80,7 +80,7 @@ class _AboutSectionState extends State<AboutSection> {
                         height: 20.0,
                       ),
                       const Text(
-                        "Technology I have worked with",
+                        "Mit diesen Technologien arbeiten wir",
                         style: TextStyle(
                           fontSize: 14,
                           fontWeight: FontWeight.bold,
